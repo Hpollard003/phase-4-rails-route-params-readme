44b6ba016f11236927173473a3c31f4cc28b539a
Hpollard003 <hpollard003@gmail.com>
2021-09-12 00:28:35 -0400

    commit tree c707c6a88532710bc6d6699f4c2825276e094f83

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -2,7 +2,12 @@ class CheesesController < ApplicationController
 
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses 
+  end
+
+  def show
+    cheeses = Cheese.find(params[:id])
+    render json:cheeses
   end
 
 end
